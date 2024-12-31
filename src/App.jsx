708f19f491b1867cc6a01b49e316bf3674613c39
abbesbilel <b_abbes@estin.dz>
2024-12-31 04:10:33 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './Header'
+import Footer from './Footer'
+import Landing from './Landing'
+import Services from './Services'
+import Team from './Team'
+import Testimonials from './Testimonials'
+
+
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <Landing/>
+     <Services/>
+     <Team/>
+     <Testimonials/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
